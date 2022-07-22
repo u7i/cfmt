402f7df1742eafd8e217db0684e38444b7fd0749
u7i <umnicode2@gmail.com>
2022-07-23 00:48:56 +0200

    - Staging commit, doesn't working.

--- test/simple_format.cpp
+++ test/simple_format.cpp
@@ -10,6 +10,7 @@ int main() {
     assert(cfmt::format("no format") == "no format");
     assert(cfmt::format("{} {} {}", 1, "abcd", 1.5f) == "1 abcd 1.5");
     assert("{} {} {}"_cfmt(1, "abcd", 1.5f) == "1 abcd 1.5");
+    assert("{a} {a}"_cfmt(1) == "1 1");
 
     std::cout << "Passed successfully: " << "{} {} {}"_cfmt(1, "abcd", 1.5f) << '\n';
     return 0;
